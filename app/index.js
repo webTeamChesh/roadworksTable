2e2642313643807e58b787a34e88f16fc6c99071
CEC WebTeam <webteam@cheshireeast.gov.uk>
2023-12-18 13:37:17 +0000

    Update index.js

--- app/index.js
+++ app/index.js
@@ -18,6 +18,7 @@ app.listen(port, (error) => {
   if (!error) console.log(`Server running on port ${port}`);
   else console.log(error);
 });
+
 //app.use(express.static(dir));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
