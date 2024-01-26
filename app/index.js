82869c7fd7b80a51df3473439d8738a8b768779d
antiloquax <tranter.m@sky.com>
2024-01-26 16:59:28 +0000

    Update index.js

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 const url =
   'https://datacloud.one.network/?app_key=94db72b2-058e-2caf-94de16536c81';
 const user = 'cheshireeast';
-const password = 'process.env.ON_PWD';
+const password = process.env.ON_PWD;
 
 //let env = [];
 //Object.keys(process.env).forEach((k) => env.push(`${k}: ${process.env[k]}`));
