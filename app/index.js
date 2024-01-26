56ef167e7f1b56951e2848f13c81ca7fb69acc97
antiloquax <tranter.m@sky.com>
2024-01-26 12:52:01 +0000

    Update index.js

--- app/index.js
+++ app/index.js
@@ -11,8 +11,7 @@ const port = process.env.PORT || 3001;
 const url =
   'https://datacloud.one.network/?app_key=94db72b2-058e-2caf-94de16536c81';
 const user = 'cheshireeast';
-const password = process.env.ON_PWD;
-console.log(password);
+const password = 'Tkfdg58F]pjA';
 
 app.listen(port, (error) => {
   if (!error) console.log(`Server running on port ${port}`);
